d7849e81e62d48d4eaf1f6aa4e3b23aba526e58f
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-07-09 01:15:27 +0800

    include react-intl

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Provider} from 'react-redux'
-import './index.css'
+import {IntlProvider} from 'react-intl'
+
 import App from './containers/App'
 import registerServiceWorker from './registerServiceWorker'
 
 render(
   <Provider>
-    <App/>
+    <IntlProvider locale="en">
+      <App/>
+    </IntlProvider>
   </Provider>,
   document.getElementById('root')
 )
---
+++ src/util/locale.js
@@ -0,0 +1,24 @@
+/**
+ * get locale resource files
+ *
+ * @param {any} lang
+ * @returns
+ */
+function getLocale(lang) {
+  let result = {}
+  switch (lang) {
+    case 'zh-CN':
+      result = require('./locales/zh-Hans')
+      break
+    case 'en-US':
+      result = require('./locales/en-US')
+      break
+    case 'ja':
+      result = require('./locales/ja')
+      break
+    default:
+      result = require('./locales/zh-Hans')
+  }
+
+  return result.default || result
+}
