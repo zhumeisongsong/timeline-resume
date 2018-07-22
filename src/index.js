175286166083f886fd8bdf55e33cfb6f6996fc03
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-07-22 18:19:53 +0800

    include react-intl

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {Provider} from 'react-redux'
-import {IntlProvider, addLocaleData} from 'react-intl'
-import LocaleProvider from './components/LocaleProvider'
 import App from './containers/App'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 
-import {getLocale} from './util/locale'
-
-const appLocale = getLocale('en-US')
-addLocaleData(...appLocale.data)
-
-render(
-  <Provider>
-    <LocaleProvider locale={appLocale}>
-      <IntlProvider
-        locale={appLocale.locale}
-        messages={appLocale.messages}
-        formats={appLocale.formats}
-      >
-        <App/>
-      </IntlProvider>
-    </LocaleProvider>
-  </Provider>,
+render(<App/>,
   document.getElementById('root')
 )
-registerServiceWorker()
+// registerServiceWorker()
