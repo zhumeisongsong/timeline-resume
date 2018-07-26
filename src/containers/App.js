473911874ff5e166128fffc72e6c7abdff68e90d
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-07-26 20:10:14 +0800

    eslint add

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
+import { IntlProvider, addLocaleData } from 'react-intl'
+import keyIndex from 'react-key-index'
+// import LocaleProvider from '@/components/locale-provider'
+// import Home from '@/views/home'
 import '../assets/stylesheet/css/style.css'
 
 import ImageItem from '../components/ImageItem'
 
-const dataArray = [
+let dataArray = [
   {
     thumbnail: 'https://zos.alipayobjects.com/rmsportal/BXJNKCeUSkhQoSS.png',
     image: 'https://zos.alipayobjects.com/rmsportal/DGOtoWASeguMJgV.png',
@@ -19,9 +23,36 @@ const dataArray = [
   }
 ]
 
+dataArray = keyIndex(dataArray, 1)
+
+/**
+ * 获取国际化资源文件
+ *
+ * @param {any} lang
+ * @returns
+ */
+function getLocale(lang) {
+  /* eslint-disable global-require */
+  let result = {}
+  switch (lang) {
+    case 'zh-CN':
+      result = require('./locales/zh-Hans')
+      break;
+    case 'en-US':
+      result = require('./locales/en-US')
+      break;
+    default:
+      result = require('./locales/zh-Hans')
+  }
+
+  return result.default || result
+  /* eslint-enable global-require */
+}
+
 
 class App extends Component {
   state = {
+    lang: 'en-US',
     projects: dataArray
   }
 
@@ -35,7 +66,7 @@ class App extends Component {
     } = this.state
     return (
       <section className="image-list">
-        {projects.map(val => <ImageItem val={val} key={val.name} />)}
+        {projects.map(val => <ImageItem val={val} key={val.id} />)}
       </section>
     )
   }
