cae0bde7a1322f80e92d0d0f100de2d33dd35962
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-08-15 20:08:14 +0800

    add stylus

--- src/components/SkillList.js
+++ src/components/SkillList.js
@@ -1,7 +1,20 @@
 import React from 'react';
 
-const SkillList = ({location}) => (
-  <div>detail</div>
-);
+const SkillList = (props) => {
+  const score =()=>{
+
+  }
+
+  return (<div className="skill-container">
+    {props.data.map((val) =>
+      <div key={val.id} className="item">
+        {console.log(val)}
+        <h2>{val.type}</h2>
+        <div>{val.score}</div>
+        <div>{val.content}</div>
+      </div>
+    )}
+  </div>)
+};
 
 export default SkillList;
