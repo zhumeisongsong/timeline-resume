9ed27045b2795d1c1d4e80ffbca18678f9b43f9e
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-08-16 18:15:48 +0800

    cover start

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -4,17 +4,19 @@ import {FormattedMessage} from 'react-intl';
 const Introduction = (props) => {
   return (
     <div className="cover-container">
-      <div>
-        <FormattedMessage
-          id='intro.hello'
-          values={{
-            name: props.data.name + (props.data.read ? `(${props.data.read})` : '')
-          }}/>
-      </div>
-      <div>{props.data.face}</div>
-      <div>{props.data.description}</div>
-      <div className="icon-item">
-        <div className="icon icon-arrow-down"/>
+      <div className="wrapper">
+        <div className="name">
+          <FormattedMessage
+            id='intro.hello'
+            values={{
+              name: props.data.name + (props.data.read ? `(${props.data.read})` : '')
+            }}/>
+        </div>
+        <h1>{props.data.face}</h1>
+        <p>{props.data.description}</p>
+        <div className="icon-item">
+          <div className="icon icon-arrow-down"/>
+        </div>
       </div>
     </div>)
 };
