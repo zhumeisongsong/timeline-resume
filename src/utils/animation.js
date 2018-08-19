9adddeece9b83b7969b8d81fd36832b68d7fd4f9
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-08-20 02:13:43 +0800

    pdf download

--- src/utils/animation.js
+++ src/utils/animation.js
@@ -1,7 +1,9 @@
 const scrollAnimation = () => {
   [].forEach.call(document.getElementsByClassName('scroll'), (el) => {
     if (document.documentElement.scrollTop + window.innerHeight >= el.offsetTop) {
-      el.className += " is-scoped";
+      if (el.className.indexOf('is-scoped') === -1) {
+        el.className += " is-scoped";
+      }
     }
   })
 };
