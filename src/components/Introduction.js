4a1a703297fee4334e3fd298d88e5fd4ad50b45a
zhumeisongsong <zhumei.song@monstar-lab-cn.com>
2018-08-19 16:52:20 +0800

    pdf download

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,25 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {FormattedMessage} from 'react-intl';
+import animation from '../utils/animation';
 
-const Introduction = (props) => {
-  return (
-    <div className="cover-container">
-      <div className="wrapper">
-        <div className="name">
-          <FormattedMessage
-            id='intro.hello'
-            values={{
-              name: props.data.name + (props.data.read ? `(${props.data.read})` : '')
-            }}/>
+class Introduction extends Component {
+  componentDidMount() {
+    window.addEventListener('scroll', animation(this.container));
+  }
 
+  render() {
+    const {
+      data
+    } = this.props
+    return (
+      <div className="cover-container scroll fade-in"
+           ref={(el) => this.container = el }>
+        <div className="wrapper">
+          <div className="name">
+            <FormattedMessage
+              id='intro.hello'
+              values={{
+                name: data.name + (data.read ? `(${data.read})` : '')
+              }}/>
+
+          </div>
+          <h1 dangerouslySetInnerHTML={{__html: data.face}}/>
+          <p>{data.description}</p>
+          <div className="icon-item">
+            <div className="icon icon-arrow-down"/>
+          </div>
         </div>
-        <h1 dangerouslySetInnerHTML={{__html: props.data.face}}/>
-        <p>{props.data.description}</p>
-        <div className="icon-item">
-          <div className="icon icon-arrow-down"/>
-        </div>
-      </div>
-    </div>)
-};
+      </div>)
+  }
+}
 
 export default Introduction;
