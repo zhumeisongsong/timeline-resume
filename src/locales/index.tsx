b69f23b6f11ca96796c231245ae978af287c3975
zhumeisongsong <zhumei.song@monstar-lab.com>
2021-07-31 23:13:11 +0900

    ✨ add localization

---
+++ src/locales/index.tsx
@@ -0,0 +1,50 @@
+import React, { useReducer } from 'react';
+import jaJP from './jaJP';
+import enUS from './enUS';
+
+export const langLocales: any = {
+  en: enUS,
+  ja: jaJP
+};
+
+const getTranslate =
+  (langCode: string) =>
+  (key: string): string =>
+    langLocales[langCode][key] || key;
+
+const initialState = {
+  langCode: 'en',
+  translate: getTranslate('en')
+};
+
+export const I18nContext = React.createContext(initialState);
+
+export default ({ children, lang }: any) => {
+  /* This is where magic starts to happen. We're creating
+  a reducer to manage the global state which will sit in
+  I18nContext. For now, the only action we will have
+  is setting language */
+  const reducer = (state: any, action: any) => {
+    switch (action.type) {
+      case 'setLanguage':
+        return {
+          langCode: action.payload,
+          translate: getTranslate(action.payload)
+        };
+      default:
+        return { ...initialState };
+    }
+  };
+
+  /* useReducer hook receives a reducer and an initialState to
+  return the current state object with a dispatch method to
+  dispatch actions. */
+
+  const [state, dispatch]: any = useReducer(reducer, { ...initialState });
+
+  return (
+    <I18nContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </I18nContext.Provider>
+  );
+};
---
+++ src/locales/jaJP.ts
@@ -0,0 +1,22 @@
+
+export default {
+  "intro.hello":"初めまして、{name}です",
+  "button.back": "戻る",
+  "list.name": "案件名",
+  "list.country":"こっか",
+  "list.type": "タイプ",
+  "list.time": "日付",
+  "list.content": "概要",
+  "list.link": "リンク",
+  "list.codeLink": "GitHubリンク",
+  "list.infrastructure": "インフラ",
+  "list.myRole": "担当フェーズ",
+  "list.skill": "使用ツール",
+  "list.team": "開発規模",
+  "list.selfResponsibility": "自分の担当",
+  "tag.code":"コードがある",
+  "tag.link":"リンクがある",
+  "modal.title":"案件詳細",
+  "file.career":"職務経歴書",
+  "file.resume":"履歴書"
+}
