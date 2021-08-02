0b9041e6f632bcef2e0f418b9617ee16d1953c84
zhumeisongsong <zhumei.song@monstar-lab.com>
2021-08-02 17:06:30 +0900

    ✨ rename files

--- src/components/Contact/index.tsx
+++ src/components/Contact/index.tsx
@@ -1,7 +1,35 @@
-import React, { useMemo, useCallback } from 'react';
+import React, { useMemo, useCallback, useContext } from 'react';
+import { I18nContext } from '../../locales';
 
 const Contact = () => {
-  return useMemo(() => <div>Contact</div>, []);
+  const { translate } = useContext(I18nContext);
+
+  return useMemo(
+    () => (
+      <section className="section-container contact-container">
+        <div className="wrapper">
+          <div className="item">
+            <a href="mailto: zhumeisongsong@gmail.com">
+              zhumeisongsong@gmail.com
+            </a>
+          </div>
+
+          <div className="item">
+            <a href={translate('file.careerPath')}>
+              {translate('file.career')}
+            </a>
+          </div>
+
+          <div className="item">
+            <a href={translate('file.resumePath')}>
+              {translate('file.resume')}
+            </a>
+          </div>
+        </div>
+      </section>
+    ),
+    []
+  );
 };
 
 export default Contact;
--- src/locales/jaJP.ts
+++ src/locales/jaJP.ts
@@ -1,13 +1,11 @@
 
 export default {
-  "intro.hello":"初めまして、宋竹梅(ソウ ジュウメイ)と申します",
+  "intro.hello": "初めまして、宋竹梅(ソウ ジュウメイ)と申します",
   "intro.face": "フロントエンドエンジニア",
   "intro.description": "description ja",
-  "resume": "./files/resume-ja.pdf",
-  "career": "./files/career-ja.pdf",
   "button.back": "戻る",
   "list.name": "案件名",
-  "list.country":"こっか",
+  "list.country": "こっか",
   "list.type": "タイプ",
   "list.time": "日付",
   "list.content": "概要",
@@ -18,9 +16,11 @@ export default {
   "list.skill": "使用ツール",
   "list.team": "開発規模",
   "list.selfResponsibility": "自分の担当",
-  "tag.code":"コードがある",
-  "tag.link":"リンクがある",
-  "modal.title":"案件詳細",
-  "file.career":"職務経歴書",
-  "file.resume":"履歴書"
+  "tag.code": "コードがある",
+  "tag.link": "リンクがある",
+  "modal.title": "案件詳細",
+  "file.career": "職務経歴書",
+  "file.resume": "履歴書",
+  "file.careerPath": "./files/職務経歴書-宋竹梅.pdf",
+  "file.resumePath": "./files/履歴書-宋竹梅.pdf",
 }
