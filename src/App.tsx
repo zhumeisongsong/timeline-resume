993711fed7064e3deba658a9a6569f32203083d7
zhumeisongsong <zhumeisongsong@gmail.com>
2023-11-22 14:58:47 +0900

    :sparkles: init vite react-typescript app

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import React, { FC, useMemo } from 'react';
-import Layout from './components/Layout';
-import Top from './containers/Top';
-import I18nContextProvider from './locales';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-import './index.css';
+function App() {
+  const [count, setCount] = useState(0)
 
-const App: FC = () => {
-  return useMemo(
-    () => (
-      <I18nContextProvider>
-        <Layout>
-          <Top />
-        </Layout>
-      </I18nContextProvider>
-    ),
-    []
-  );
-};
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
